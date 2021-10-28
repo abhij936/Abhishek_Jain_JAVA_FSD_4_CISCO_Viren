a49f64805c2dbcb6f2e1b98767a36ce1dafb52d4
Abhishek Jain <abhij936@gmail.com>
2021-10-28 21:34:00 +0530

    online test application

---
+++ src/js/quiz.js
@@ -0,0 +1,109 @@
+let quiz=[
+    {
+        question:"Which of these methods of class StringBuffer is used to extract a substring from a String object?",
+        option:[
+            "1.substring()",
+            "2.Substring()",
+            "3.SubString()",
+            "4.None of the mentioned",
+        ],
+        answer:1,
+    },
+    {
+        question:"Which of these can be overloaded?",
+        option:[
+            "1.Methods",
+            "2.Constructors",
+            "3.All of the mentioned",
+            "4.None of the mentioned",
+            
+        ],
+        answer:3,
+    },
+    {
+        question:"Which of these process occur automatically by java run time system?",
+        option:[
+            "1.Serialization",
+            "2.Memory allocation",
+            "3.Deserialization",
+            "4.All of the mentioned",   
+        ],
+        answer:4,
+    },
+    {
+        question:"Which of these interface handle sequences?",
+        option:[
+            "1.Set",
+            "2.List",
+            "3.Comparator",
+            "4.Collection",   
+        ],
+        answer:2,
+
+    },
+    {
+        question:"Which of the following classes can catch all exceptions which cannot be caught?",
+        option:[
+            "1.RuntimeException",
+            "2.Error",
+            "3.Exception",
+            "4.ParentException",   
+        ],
+        answer:2,
+    },
+    {
+        question:"What decides thread priority?",
+        option:[
+            "1.Process",
+            "2.Process scheduler",
+            "3.Thread",
+            "4.Thread scheduler",
+            
+        ],
+        answer:4,
+    },
+    {
+        question:"Which of these packages contains all the classes and methods required for event handling in Java?",
+        option:[
+            "1.java.applet",
+            "2.java.awt",
+            "3.java.event",
+            "4.java.awt.event",
+            
+        ],
+        answer:4,
+    },
+    {
+        question:"Which file separator should be used by MANIFEST file?",
+        option:[
+            "1. /",
+            "2. \\",
+            "3. -",
+            "4. //",
+            
+        ],
+        answer:1,
+    },
+    {
+        question:"Which of the below is not a session tracking method?",
+        option:[
+            "1.URL rewriting",
+            "2.History",
+            "3.Cookies",
+            "4.SSL sessions",
+            
+        ],
+        answer:2,
+    },
+    {
+        question:"Which file is used to define dependency in maven?",
+        option:[
+            "1.build.xml",
+            "2.pom.xml",
+            "3.dependency.xml",
+            "4.version.xml",
+            
+        ],
+        answer:2,
+    }
+]
